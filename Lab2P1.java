87fcad54dc5c5fca92f675e3869814b82f91a1fb
priyabharathikoppisetti-arch <priyabharathikoppisetti@gmail.com>
2025-09-10 10:40:34 +0530

    Add files via upload

---
+++ Lab2P1.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+class Lab2P1{
+	public static void main(String...args){
+	  Scanner s = new Scanner(System.in);
+	  int a = s.nextInt();
+	  //s.nextLine();
+	  String op = s.next();
+	  
+	  switch(op){
+	  	case"++":
+	  		System.out.println("post:"+(a++));
+	  		System.out.println("pre:"+(++a));
+	  		break;
+	  	case"--":
+	  		System.out.println("post:"+(a--));
+	  		System.out.println("pre:"+(--a));
+	  		break;
+	  	
+	  	case"+":
+	  		a = +a;
+	  		System.out.println("unary +: "+(a));
+	  		break;
+	  		
+	  	case"-":
+	  		a = -a;
+	  		System.out.println("unary -: "+(a));
+	  		break;
+	  	default:
+	  		System.out.println("OPERATOR DOES NOT EXIST");
+	  		
+	  }
+	}
+}
