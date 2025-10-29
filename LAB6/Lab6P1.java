0045e29e449eb7caffbc4af5ac4977c2dfdcf83a
priyabharathikoppisetti-arch <priyabharathikoppisetti@gmail.com>
2025-10-29 12:04:57 +0530

    Create Lab6P1.java

---
+++ LAB6/Lab6P1.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+
+enum Fruits{
+	
+	Apple('a'),Apricot('a'),Banana('b'),citrus('c'),Fig('f'),Grapes('g');
+	
+	char a;
+	
+	Fruits(char a){
+		this.a = a;
+	}
+	public char get(){
+		return a;
+	}
+}
+
+public class EnumEx1{
+	public static void main(String...args){
+		Scanner s = new Scanner(System.in);
+		String st = s.next();
+		char ch = st.charAt(0);
+		ch = Character.toLowerCase(ch);
+		 for(Fruits f: Fruits.values())
+		 {
+		 	if(f.get() == ch)
+		 	System.out.println(f);
+		 }
+	}
+}
